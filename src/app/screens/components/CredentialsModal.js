140a10481a616cfcaa31ee5946216a382211e120
Jonathan Cooper <cooper@cooperthecoder.com>
2020-07-16 10:54:42 -0500

    Added link to wireguard client on credentials modal

--- src/app/screens/components/CredentialsModal.js
+++ src/app/screens/components/CredentialsModal.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import QRCode from 'qrcode.react'
 
+
 class CredentialsModal extends Component {
     render() {
         return (
@@ -18,7 +19,7 @@ class CredentialsModal extends Component {
                 <Modal.Body>
                     <Alert
                         variant="warning">
-                            This is your Wireguard private key.
+                            This is your <Alert.Link href="https://www.wireguard.com/install/" target="_blank" rel="noopener noreferrer">Wireguard</Alert.Link> private key.
                             For your safety, you won't be able to download it again.
                             Store it in a <strong>safe</strong> place.
                     </Alert>
@@ -28,9 +29,10 @@ class CredentialsModal extends Component {
                         style={{margin: "0 auto", display: "block"}} />
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={this.props.onClose}>
+                    <Button variant="danger" onClick={this.props.onClose}>
                         Close
                     </Button>
+
                     <Button variant="success" onClick={this.props.onDownload}>Download</Button>
                 </Modal.Footer>
             </Modal>
