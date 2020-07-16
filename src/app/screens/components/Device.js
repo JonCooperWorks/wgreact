0cda2d4e5eb172b2bfc602defea7310c2707a158
Jonathan Cooper <cooper@cooperthecoder.com>
2020-07-16 13:12:40 -0500

    Neatened up device cards

--- src/app/screens/components/Device.js
+++ src/app/screens/components/Device.js
@@ -9,14 +9,17 @@ class Device extends Component {
 
     render() {
         return (
-            <Card>
-                <Card.Title>
-                    <OSBadge OS={this.props.device.OS} />
-                    {this.props.device.Name}
-                </Card.Title>
-                <Card.Subtitle
-                    className="text-muted"
-                    style={{marginLeft: "5px"}}>{this.props.device.OS}</Card.Subtitle>
+            <Card
+                style={{margin: "10px"}}>
+                <Card.Header>
+                    <Card.Title>
+                        <OSBadge OS={this.props.device.OS} />
+                        {this.props.device.Name}
+                    </Card.Title>
+                    <Card.Subtitle
+                        className="text-muted"
+                        style={{marginLeft: "5px"}}>{this.props.device.OS}</Card.Subtitle>
+                </Card.Header>
                 <Card.Body>
                     <CopyableValue
                         label="Public Key"
@@ -26,17 +29,21 @@ class Device extends Component {
                         label="IP Address"
                         value= {this.props.device.IPAddress} />
 
+                </Card.Body>
+                <Card.Footer>
                     <Button 
+                        style={{marginRight: "5px"}}
                         variant="warning"
                         onClick={this.props.onRekeyDevice}>
                             Regenerate Credentials
                         </Button>
                     <Button 
+                        style={{marginLeft: "5px"}}
                         variant="danger"
                         onClick={this.props.onDeleteDevice}>
                             Revoke Access
                     </Button>
-                </Card.Body>
+                </Card.Footer>
             </Card>
         )
     }
